3a046ab9539bc10d525da70b9bbc33e05df279a4
joakimdeak <joakimdeak@hotmail.com>
2022-04-28 13:01:34 +0200

    Login only with google

--- process.d.ts
+++ process.d.ts
@@ -1,14 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    GITHUB_ID: string;
-    GITHUB_SECRET: string;
-    FACEBOOK_ID: string;
-    FACEBOOK_SECRET: string;
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
-    APPLE_ID: string;
-    APPLE_TEAM_ID: string;
-    APPLE_PRIVATE_KEY: string;
-    APPLE_KEY_ID: string;
   }
 }
