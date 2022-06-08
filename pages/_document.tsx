f55e6314c7752de59088f2d8330f1080fc75a292
joakimdeak <joakimdeak@hotmail.com>
2022-06-08 10:59:25 +0200

    Add google verification

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="7lcGi4T04vrpeFK8qkAH0tawPidaRLeVxReu3--B1VI" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
