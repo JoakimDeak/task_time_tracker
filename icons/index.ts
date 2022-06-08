178ffd5513396fd5d059e7fc9b7342780b6fbe0d
joakimdeak <joakimdeak@hotmail.com>
2022-06-08 09:51:17 +0200

    add icons

--- icons/index.ts
+++ icons/index.ts
@@ -1,7 +1,10 @@
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { default as PlayIcon } from '@mui/icons-material/PlayArrow';
+import PlayIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PrivacyIcon from '@mui/icons-material/PrivacyTip';
+import KebabMenuIcon from '@mui/icons-material/MoreVert';
 
-export { EditIcon, CheckIcon, DeleteIcon, PlayIcon, PauseIcon };
+export { EditIcon, CheckIcon, DeleteIcon, PlayIcon, PauseIcon, LogoutIcon, PrivacyIcon, KebabMenuIcon };
